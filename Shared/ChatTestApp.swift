f7ac7d3321a40214ccdc3214f0c82dbdc8b0e7c4
Mikhail <wowivan2011@yandex.ru>
2021-06-29 11:03:46 +0300

    Initial Commit

---
+++ Shared/ChatTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatTestApp.swift
+//  Shared
+//
+//  Created by Mikhail Ivanov on 29.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChatTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
